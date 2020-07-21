3d01cc0df84f92670dd6887e03c37fc920a392d4
Lucas Segurado <segurado1996@gmail.com>
2020-07-21 18:53:09 -0300

    Removed repeated code

--- src/cssAsset.ts
+++ src/cssAsset.ts
@@ -1,5 +1,5 @@
 const Asset = require('parcel-bundler/src/Asset.js');
-const fs: typeof import('fs') = require('fs');
+const convertCssStringIntoJsModule = require('@lsegurado/ls-convert-css-to-js-module/bin/convertCssStringIntoJsModule');
 
 class cssAsset extends Asset {
     constructor(...args) {
@@ -7,27 +7,10 @@ class cssAsset extends Asset {
         this.type = 'js';
     }
     parse(style: string) {
-        const regexImports = new RegExp(/@import '[^']*';|@import "[^"]*";/, 'g');
-        const imports = style.match(regexImports);
-        const importsFormatted = imports ? imports.map((value, index) => value.replace('@import', `import style${index} from `)) : [];
-        const styleWithoutImports = style.replace(regexImports, '');
-
-        let resultString = '';
-        if (importsFormatted.length > 0) {
-            importsFormatted.forEach(importFormatted => resultString = resultString.concat(importFormatted));
-            resultString = resultString.concat(`let style = '${this.minifyString(styleWithoutImports)}';`);
-            importsFormatted.forEach((_value, index) => resultString = resultString.concat(`style = style.concat(style${index});`));
-            resultString = resultString.concat(`export default style;`);
-        } else {
-            resultString = resultString.concat(`export default '${this.minifyString(styleWithoutImports)}';`);
-        }
-        return resultString;
+        return convertCssStringIntoJsModule(style);
     }
     generate() {
         return this.ast;
-    }
-    minifyString(string) {
-        return string.replace(/\s+/g, ' ').trim();
     }
 }
 module.exports = cssAsset;
